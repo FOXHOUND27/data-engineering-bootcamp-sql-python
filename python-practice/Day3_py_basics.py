235e1c5a969fdb4c6070021cd772f84a4d30fdf8
dalglish <dalglish45@gmail.com>
2025-04-20 10:36:40 +0200

    adding completed mark task function

---
+++ python-practice/Day3_py_basics.py
@@ -0,0 +1,36 @@
+# To do list cli app built using python
+
+# Creating dictionary to store to do items
+tasks = {}
+
+# Creating a funnction to add a task
+
+
+def addTask(name, taskNumber, isComplete=False):
+    task = {"name": name, "Complete": isComplete}
+    tasks[taskNumber] = task
+
+# Creating a function to remove task using task number
+
+
+def removeTask(taskNumber):
+    tasks.pop(taskNumber)
+
+
+# Creating a function to mutate the complete state
+def markTask(taskNumber):
+    if taskNumber in tasks:
+        tasks[taskNumber]["Complete"] = True
+        print("Task Marked as complete")
+    else:
+        print("Task not found")
+
+
+addTask("Clean House", 1)
+addTask("Bath Dog", 2)
+addTask("Iron Clothes", 3)
+
+markTask(1)
+markTask(2)
+
+print(tasks)
